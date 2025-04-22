c00f861732b7b6cf7fe2da6df1a952f59e37d3f0
LI8945 <lima.ibrahimkhail@sjsu.edu>
2025-04-21 21:57:22 -0700

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,38 +1,28 @@
 from turtle import Turtle
 
-# Font style for the scoreboard
 FONT = ("Courier", 24, "normal")
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.level = 1
-        self.setup_appearance()
-        self.display_level()
-
-    def setup_appearance(self):
-        # Set appearance of the scoreboard turtle
         self.color("black")
         self.penup()
         self.hideturtle()
+        self.update_score()
 
-    def display_level(self):
-        # Display the current level on the screen
+
+    def update_score(self):
         self.clear()
         self.goto(-230, 250)
         self.write(f"Level: {self.level}", align="Center", font=FONT)
 
-    def increase_level(self):
-        # Increase level when the player crosses
+    def score(self):
         self.level += 1
-        self.display_level()
+        self.update_score()
 
-    def show_game_over(self):
-        # Display game over message at the center
-        self.goto(0, 0)
-        self.write("GAME OVER", align="Center", font=FONT)
-
-
-
-
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align="Center", font=FONT)
